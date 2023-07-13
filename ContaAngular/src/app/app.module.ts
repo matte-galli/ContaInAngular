43002890e786340a524a6fca6a2a0224ad5f43d0
MatteoGalli <matteogalli.sg@gmail.com>
2023-07-13 15:32:56 +0200

    Fine?

--- ContaAngular/src/app/app.module.ts
+++ ContaAngular/src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Componente1Component } from './componente1/componente1.component';
 import { Componente2Component } from './componente2/componente2.component';
 import { Componente3Component } from './componente3/componente3.component';
 import { Componente4Component } from './componente4/componente4.component';
+import { SharedService } from './shared.service';
 
 @NgModule({
   declarations: [
@@ -17,10 +17,9 @@ import { Componente4Component } from './componente4/componente4.component';
     Componente4Component
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
